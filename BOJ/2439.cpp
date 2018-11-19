df3e2c0c0964aa18b6e724c94e6b91873f314a83
artium59@gmail.com <artium59@gmail.com>
2018-11-20 03:15:24 +0900

    Add 2439 Draw stars - 2

---
+++ BOJ/2439.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+int main() {
+    int n, j = 1;
+    scanf("%d", &n);
+    
+    while (1) {
+        for (int i = 0; i < n-j; i++)
+            printf(" ");
+        
+        for (int i = 0; i < j; i++)
+            printf("*");
+        
+        printf("\n");
+        j++;
+        
+        if (j == n+1) break;
+    }
+    
+    return 0;
+}
