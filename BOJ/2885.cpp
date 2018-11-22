0177b31cd0d066acd15d968a7ae25d07301e989b
artium59@gmail.com <artium59@gmail.com>
2018-11-23 04:52:45 +0900

    Add 2885 COKOLADA

---
+++ BOJ/2885.cpp
@@ -0,0 +1,25 @@
+﻿#include <bits/stdc++.h>
+
+int main() {
+    // 1 ≤ K ≤ 1,000,000
+    int k, ans = 1, split = 0;
+    scanf("%d", &k);
+    
+    while (k > ans)
+        ans = ans << 1;
+    
+    printf("%d ", ans);
+    
+    while (ans > 0) {
+        if (k == ans) break;
+        
+        ans = ans >> 1;
+        split++;
+        if (k > ans)
+            k -= ans;
+    }
+    
+    printf("%d", split);
+    
+    return 0;
+}
